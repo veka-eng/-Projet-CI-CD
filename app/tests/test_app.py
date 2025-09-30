54868be2294ac08189428a367e5bdb3f90f0fc3d
veka-eng <v.madiona@ecole-ipssi.net>
2025-09-30 20:50:41 +0200

    Delete app directory

--- app/tests/test_app.py
+++
@@ -1,7 +0,0 @@
-from app import app
-
-def test_root():
-    client = app.test_client()
-    res = client.get("/")
-    assert res.status_code == 200
-    assert "Deployed via Ansible" in res.get_json()["message"]
